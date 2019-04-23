3aa029b3951dbb151c5c6ed3064f5f5bd89b4413
Craig Tataryn <craig@grindsoftware.com>
2019-04-23 14:39:53 -0500

    Added new INSPECTLET_JS_SERVER and INSPECTLET_JS_URI settings
    These allow you to configure where you want to load `inspectlet.js` from.
    I use inspectlet on my beta site, and often I get freelancers to test my
    features before deploying them to prod. The problem is, I can never be
    assured that the freelancer isn't running an Ad Blocker extension which
    blocks Inspectlet.
    
    Using these new configuraiton settings I can have a version of the
    inspectlet.js file loaded from my own domain, thus the Ad Blocker will
    not block it.

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ module.exports = {
   name: 'ember-cli-inspectlet',
 
   contentFor: function(type, config) {
-    var wid = config.APP.INSPECTLET_WID;
+    const wid = config.APP.INSPECTLET_WID;
+    const js_server = config.APP.INSPECTLET_JS_SERVER ? config.APP.INSPECTLET_JS_SERVER : 'cdn.inspectlet.com';
+    const js_uri = config.APP.INSPECTLET_JS_URI ? config.APP.INSPECTLET_JS_URI : 'inspectlet.js';
 
     if (wid != null && type === 'head') {
       return "<script type='text/javascript' id='inspectletjs'>\n" +
         "window.__insp = window.__insp || [];\n" +
         "__insp.push(['wid', " + wid + "]);\n" +
         "(function() {\n" +
-        "function ldinsp(){if(typeof window.__inspld != 'undefined') return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = 'inspsync'; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };\n" +
+        "function ldinsp(){if(typeof window.__inspld != 'undefined') return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = 'inspsync'; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://" + js_server + "/" + js_uri + "'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };\n" +
         " setTimeout(ldinsp, 500); document.readyState != 'complete' ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();\n" +
         "})();" +
         "</script>"
